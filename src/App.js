9932b88a47cda84f942c771e2b3229975ce33867
sayyad azeem <azeemsayyd1652@gmail.com>
2023-12-13 00:11:25 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Route,  Routes } from "react-router-dom";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      Hello This is the react app, build for testing the nginx :)
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <div className="App">
+        Hello This is the react app, build for testing the nginx :)
+      </div>
+    </BrowserRouter>
   );
 }
 
